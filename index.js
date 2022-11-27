5a5f52e27a2a61319d6400e6951421182bf6f295
Kinshu Jain <kinshujain0@gmail.com>
2022-11-28 00:17:39 +0530

    animation added

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ for (var i = 0; i < n; i++)
 function handleClick() {
     var x = this.innerHTML;
 
+    makeSound(x);
+
+    makeAnimation(x);
+
+}
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
+
+
+document.addEventListener("keypress", function (event) {
+    // console.log(eFvent.code);
+    var x = event.key;
+
+    x = x.toLowerCase();
+
+    makeSound(x);
+    makeAnimation(x);
+
+});
+
+function makeSound(x) {
     switch (x) {
         case "w":
             var audio = new Audio('sounds/w.mp3');
@@ -43,45 +65,14 @@ function handleClick() {
     }
 }
 
-// var audio = new Audio('sounds/tom-1.mp3');
-// audio.play();
 
+function makeAnimation(x) {
+    var button = document.querySelector("." + x);
 
-document.addEventListener("keypress", function(event){
-    // console.log(eFvent.code);
-    var x = event.code;
-    switch (x) {
-        case "KeyW":
-            var audio = new Audio('sounds/w.mp3');
-            audio.play();
-            break;
+    button.classList.add("pressed");
 
-        case "KeyA":
-            var audio = new Audio('sounds/a.mp3');
-            audio.play();
-            break;
-
-        case "KeyS":
-            var audio = new Audio('sounds/s.mp3');
-            audio.play();
-            break;
-        case "KeyD":
-            var audio = new Audio('sounds/d.mp3');
-            audio.play();
-            break;
-        case "KeyJ":
-            var audio = new Audio('sounds/j.mp3');
-            audio.play();
-            break;
-        case "KeyK":
-            var audio = new Audio('sounds/k.mp3');
-            audio.play();
-            break;
-        case "KeyL":
-            var audio = new Audio('sounds/l.mp3');
-            audio.play();
-            break;
-        default:
-            break;
-    }
-});
+    setTimeout(function()
+    {
+        button.classList.remove("pressed");
+    },100);
+}
